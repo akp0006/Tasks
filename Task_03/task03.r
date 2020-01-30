e5401f6aa49cb1371766626d07f47022ed3b4dd3
akp0006 <59849787+akp0006@users.noreply.github.com>
2020-01-29 19:45:11 -0500

    Abbey Parmer Task 03 + Extra Credit

--- Task_03/task03.r
+++ Task_03/task03.r
@@ -78,3 +78,47 @@ plot(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime3)), nspeciesOver
 lines(seq(to=MaxMA, from=MinMA, length.out=nrow(nspeciesOverTime4)), nspeciesOverTime4[,2], col= Col_gastropod, lwd= LineWidth)
 legend("topleft", legend=c(Taxon3, Taxon4), col=c(Col_echinoderm, Col_gastropod), bty="n", lwd=LineWidth)
 #Hypothesis: An event approximately 30 million years ago led to the sharp decline of echinoderm and gastropod species, whose numbers have not recovered to this day.
+
+#Extra Credit
+install.packages("rebird")
+setwd("C:\\Users\\Abbey\\Desktop\\Evolution\\Tasks\\Task_03")
+library(rebird)
+birds <- ebirdfreq("states", "US-WV", 2019, 2019, 1, 12)
+a.goldfinch <- which(birds[,1] == "American Goldfinch")
+a.gold <- birds[a.goldfinch,]
+firstWeek <- a.gold[a.gold$monthQt %in% c("January-1", "February-1", "March-1", "April-1", "May-1", "June-1", "July-1", "August-1", "September-1", "October-1", "November-1", "December-1"),]
+crows <- which(birds[,1] == "American Crow")
+a.crow <- birds[crows,]
+firstWeekCrow <- a.crow[a.crow$monthQt %in% c("January-1", "February-1", "March-1", "April-1", "May-1", "June-1", "July-1", "August-1", "September-1", "October-1", "November-1", "December-1"),]
+AcceptDataFinch <- read.table(text="
+ monthQt     frequency
+ 1 January-1       0.244
+ 2 February-1      0.242
+ 3 March-1         0.263
+ 4 April-1         0.321
+ 5 May-1           0.346
+ 6 June-1          0.376
+ 7 July-1          0.457
+ 8 August-1        0.596
+ 9 September-1     0.548
+10 October-1       0.295
+11 November-1      0.336
+12 December-1      0.370", stringsAsFactors=FALSE)
+plot(frequency~factor(monthQt, levels=c("January-1", "February-1", "March-1", "April-1", "May-1", "June-1", "July-1", "August-1", "September-1", "October-1", "November-1", "December-1")), AcceptDataFinch, type="p", pch=15, las=2, 
+     xlab="", main="Frequencies in WV during the first week of each month in 2019")
+AcceptDataCrow <- read.table(text="
+ monthQt     frequency
+ 1 January-1       0.389
+ 2 February-1      0.437
+ 3 March-1         0.426
+ 4 April-1         0.410
+ 5 May-1           0.360
+ 6 June-1          0.382
+ 7 July-1          0.394
+ 8 August-1        0.438
+ 9 September-1     0.455
+10 October-1       0.451
+11 November-1      0.498
+12 December-1      0.387", stringsAsFactors=FALSE)
+points(frequency~factor(monthQt, levels=c("January-1", "February-1", "March-1", "April-1", "May-1", "June-1", "July-1", "August-1", "September-1", "October-1", "November-1", "December-1")), AcceptDataCrow, type = "p", pch=0, col = "red")
+legend("topleft", legend=c("Am. Goldfinch", "Am. Crow"), fill=c("black", "red"), cex=0.5, bty="n")
